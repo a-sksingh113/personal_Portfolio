a961e7dda1dd624ae5c2d002da99d8105c9fce81
Satish Singh <i.sksingh113@gmail.com>
2025-07-18 19:10:14 +0530

    1st commmit

---
+++ Satish_Prtfolio/data/index.ts
@@ -0,0 +1,288 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Bachelor of Technology in Information Technology,IIEST Shibpur",
+    description: " ",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Open for freelance work",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Favorselect - E-commerce website",
+    des: " A full-featured e-commerce website for Turkish clients.",
+    img: "/p1.png",
+    iconLists: [
+      "/next.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/nodejs.svg",
+      "/fm.svg",
+      "/mysql.svg",
+      "/aws.svg",
+      "/stripe.svg",
+    ],
+    link: "https://favorselect.com",
+  },
+  {
+    id: 2,
+    title: "JobHunt - Find Jobs & Hire Talent",
+    des: "JobHunt is a platform where job seekers find opportunities and employers easily post jobs to hire top talent.",
+    img: "/p2.png",
+    iconLists: [
+      "/re.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/nodejs.svg",
+      "/mongodb.svg",
+      "/cloud.svg",
+      "/redis.svg",
+    ],
+    link: "https://satishdev.me",
+  },
+  {
+    id: 3,
+    title: "Gausej - Tech startup platform for entrepreneurs",
+    des: "Gausej is a tech startup platform designed to empower entrepreneurs by providing the tools, resources, and support they need to launch and grow their ventures.",
+    img: "/p3.png",
+    iconLists: [
+      "/re.svg",
+      "/tail.svg",
+      "/js.svg",
+      "/mongodb.svg",
+      "/nodejs.svg",
+      "/cloud.svg",
+      "/redis.svg",
+    ],
+    link: "https://gausej.tech",
+  },
+  {
+    id: 4,
+    title: "Zaroorat - A crowdfunding platform for UAE client",
+    des: "Zarrorat is a crowdfunding platform tailored for UAE client, enabling individuals and organizations to raise funds for meaningful causes and innovative projects.",
+    img: "/p4.png",
+    iconLists: [
+      "/re.svg",
+      "/tail.svg",
+      "/js.svg",
+      "/mongodb.svg",
+      "/nodejs.svg",
+      "/cloud.svg",
+    ],
+    link: "https://www.zaroorat.xyz",
+  },
+  {
+    id: 5,
+    title: "Icche - Website for my college society at IIEST, Shibpur",
+    des: "Icche is the official website of our college society at IIEST, Shibpur, showcasing our initiatives, events, and efforts to educate and support underprivileged children through evening school programs.",
+    img: "/p5.png",
+    iconLists: [
+      "/re.svg",
+      "/tail.svg",
+      "/js.svg",
+      "/mongodb.svg",
+      "/nodejs.svg",
+      "/cloud.svg",
+      "/redis.svg",
+    ],
+    link: "https://icche.vercel.app",
+  },
+  {
+    id: 6,
+    title:
+      "ProbateForm - Document generation platform for Bombay and Delhi High Court",
+    des: "ProbateForm is a document generation platform tailored for the Bombay and Delhi High Courts, streamlining the creation of legal probate documents with accuracy and efficiency.",
+    img: "/p6.png",
+    iconLists: [
+      "/re.svg",
+      "/tail.svg",
+      "/js.svg",
+      "/mongodb.svg",
+      "/nodejs.svg",
+      "/cloud.svg",
+    ],
+    link: "https://probateform.pixbit.me",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Satish was an absolute pleasure. His professionalism and commitment to delivering high-quality work were evident throughout our two-month project. He played a key role in developing and refining the FavorSelect website.",
+    name: "Demir",
+    title: "Founder of Favorselect - E-commerce site",
+  },
+  {
+    quote:
+      "Satish delivered an impressive job portal (JobHunt) as part of an assignment we provided. The platform was thoughtfully designed, with seamless user roles for job seekers, employers, and admins.",
+    name: "Mehul Mohan",
+    title: "Founder of Fermion.app",
+  },
+  {
+    quote:
+      "Working with Satish on the Gausej platform was a smooth and rewarding experience. He understood our vision clearly and translated it into a modern, responsive, and user-friendly website. ",
+    name: "Shubham",
+    title: "Founder of Gausej - Platform for entrepreneurs ",
+  },
+  {
+    quote:
+      "Satish did an excellent job developing the Zarrorat crowdfunding platform for our UAE-based initiative. He brought our concept to life with a clean, functional, and scalable solution. ",
+    name: "Saad Muhammad",
+    title: "Founder and CEO of Zaroorat - Crowd funding platform",
+  },
+  {
+    quote:
+      "Satish did a fantastic job building the official website for our college society, Icche, at IIEST Shibpur. He beautifully captured the spirit of our initiative, showcasing our events, volunteers, and efforts to support underprivileged children. ",
+    name: "Dr. Vineet Srivastava",
+    title: "FIC of ICCHE",
+  },
+  {
+    quote:
+      "Satish played a key role in developing ProbateForm, a document generation platform tailored for the Bombay and Delhi High Courts. He delivered a secure, efficient, and user-friendly solution that simplifies the creation of legal probate documents.",
+    name: "Ashish",
+    title: "Founder of Probateform ",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "AWS",
+    img: "/aws.svg",
+    nameImg: "/awsName.png",
+  },
+  {
+    id: 3,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Backend Led",
+    desc: "Assisted in developing a dynamic web platform using Node.js, focusing on building robust backend functionality and enhancing API performance and interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Led college society project",
+    desc: "Designed and developed college society website using ReactJs, NodeJs, MongoDB, Cloudinary.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance Web Dev Project",
+    desc: "Led the dev of a web app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Led Startup Project",
+    desc: "Led and developed user-facing features and backend functionalities using modern frontend and backend technologies, ensuring seamless end-to-end application performance",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/a-sksingh113",
+  },
+  {
+    id: 2,
+    img: "/reddit.svg",
+    link: "https://www.reddit.com/u/One-Flight-6025/s/nJyK1Dq0yE",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/satish-singh-8b1786273",
+  },
+  {
+    id: 4,
+    img: "/leetcode.svg",
+    link: "https://leetcode.com/u/sksingh113",
+  },
+  {
+    id: 5,
+    img: "/codeforces.png",
+    link: "https://codeforces.com/profile/a_satish113",
+  },
+];
